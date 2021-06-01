72b866444d056c6957adab598884f9b48f987023
Alex Votyakov <schura.votyakov@yandex.ru>
2021-06-01 16:49:13 +0300

    Готовы вариант

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 let contactActive = document.querySelector('.contact__active');
-let close = document.querySelector('.close');
 let formActive = document.querySelector('.form__active');
-let displayStatus = window.getComputedStyle(formActive).display;
+let arrowRotate = document.querySelector('.contact__arrow');
 let burgerMenu = document.querySelector('.burger__menu');
 let nav = document.getElementById('nav');
 let banner = document.querySelector('.banner');
 
 contactActive.addEventListener('click', function(event) {
-    /*if (displayStatus =='none'){
-    	 formActive.style.display = 'block';
-    }*/
-    formActive.style.display = 'block'
-
+    formActive.classList.toggle('show');
+    arrowRotate.classList.toggle('arrow__rotate')
 })
 
-close.addEventListener('click', function(event) {
-    /*if(formActive.style.display == 'block'){
-    	 formActive.style.display = 'none';
-    }*/
-    formActive.style.display = 'none';
-})
 
 burgerMenu.addEventListener('click', function(event) {
-    nav.classList.add('show');
+    nav.classList.toggle('show');
 })
 
